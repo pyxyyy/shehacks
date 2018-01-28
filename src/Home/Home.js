06866dbf695fddf992f984920d51e1a35cd378bb
Lee Pei Xuan <lee.pei.xuan.2011@vjc.sg>
2018-01-27 23:07:04 -0500

    fix merge conflicts

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,14 +7,81 @@ class Home extends Component {
     constructor(props) {
         super();
         this.state = {
+            selected: 2
         }
     }
+
+    setSelected = (idx) => {
+        this.setState({
+            selected: idx
+        })
+    };
+
+    recycle = () => {
+
+    };
+
+    renderPersonalStats = () => {
+        return <div>personal stats</div>
+    };
+
+    renderTeamStats = () => {
+        return <div>team stats</div>
+    };
+
+    renderRecycle = () => {
+        return <div className="Recycle">
+            <button className="Recycle-button"
+                    onClick={() => {this.recycle()}}
+                    onKeyDown={(e) => { if (e.keyCode === 13) this.recycle(); }}
+            >HELP SAVE THE EARTH</button>
+        </div>
+    };
+
+    renderRewards = () => {
+        return <div className="Recycle">
+            <span className="Recycle-title">Logo</span>
+        </div>
+    };
+
+    renderAccount = () => {
+        return <div className="Recycle">
+            <div className="Recycle-title">
+                <img className="Recycle-avatar" src={require('./avatar.svg')} />
+            </div>
+            <div className="Recycle-display-name">
+                <span className="Recycle-name">Jane Doe</span>
+                <img className="Recycle-edit" src={require('./edit.svg')} />
+            </div>
+            <div className="Recycle-controls">
+                <span></span>
+                <span>Settings</span>
+                <span>Support</span>
+                <span>Logout</span>
+            </div>
+
+        </div>
+    };
+
+    renderPage = () => {
+        if (this.state.selected === 0)
+            return this.renderPersonalStats();
+        else if (this.state.selected === 1)
+            return this.renderTeamStats();
+        else if (this.state.selected === 2)
+            return this.renderRecycle();
+        else if (this.state.selected === 3)
+            return this.renderRewards();
+        else
+            return this.renderAccount();
+    };
 
     render() {
         return (
             <div className="Home">
                 <HeaderBar/>
-                <NavBar/>
+                {this.renderPage()}
+                <NavBar setSelected={this.setSelected} selected={this.state.selected}/>
             </div>
         );
     }
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -4,9 +4,6 @@ import './NavBar.css';
 class NavBar extends Component {
     constructor(props) {
         super();
-        this.state = {
-            selected: 2
-        }
     }
 
     render() {
@@ -14,45 +11,35 @@ class NavBar extends Component {
             <table className="NavBar-table">
                 <tbody>
                     <tr>
-                        <td align="center">
-                            {this.state.selected === 0
-                                ? <img className="NavBar-icon selected" src={require('./stats.svg')} />
+                        <td align="center" onClick={() => {this.props.setSelected(0)}}>
+                            {this.props.selected === 0
+                                ? <div className="selected"><img className="NavBar-icon" src={require('./stats.svg')} /></div>
                                 : <img className="NavBar-icon" src={require('./stats.svg')} />}
                         </td>
-                        <td align="center">
-                            {this.state.selected === 1
-                                ? <img className="NavBar-icon selected" src={require('./trophy.svg')} />
+                        <td align="center" onClick={() => {this.props.setSelected(1)}}>
+                            {this.props.selected === 1
+                                ? <div className="selected"><img className="NavBar-icon" src={require('./trophy.svg')} /></div>
                                 : <img className="NavBar-icon" src={require('./trophy.svg')} />}
                         </td>
-                        <td align="center">
-                            {this.state.selected === 2
-                                ? <div className="selected" cellPadding={0} cellSpacing={0}><img className="NavBar-icon" src={require('./recycle.svg')} /></div>
+                        <td align="center" onClick={() => {this.props.setSelected(2)}}>
+                            {this.props.selected === 2
+                                ? <div className="selected"><img className="NavBar-icon" src={require('./recycle.svg')} /></div>
                                 : <img className="NavBar-icon" src={require('./recycle.svg')} />}
                         </td>
-                        <td align="center">
-                            {this.state.selected === 3
-                                ? <img className="NavBar-icon selected" src={require('./cash.svg')} />
+                        <td align="center" onClick={() => {this.props.setSelected(3)}}>
+                            {this.props.selected === 3
+                                ? <div className="selected"><img className="NavBar-icon" src={require('./cash.svg')} /></div>
                                 : <img className="NavBar-icon" src={require('./cash.svg')} />}
                         </td>
-                        <td align="center">
-                            {this.state.selected === 4
-                                ? <img className="NavBar-icon selected" src={require('./account.svg')} />
+                        <td align="center" onClick={() => {this.props.setSelected(4)}}>
+                            {this.props.selected === 4
+                                ? <div className="selected"><img className="NavBar-icon" src={require('./account.svg')} /></div>
                                 : <img className="NavBar-icon" src={require('./account.svg')} />}
                         </td>
                     </tr>
                 </tbody>
             </table>
         );
-
-        {/*return (
-            <div className="NavBar">
-                <img className={iconStyle} src={require("./stats.svg")} />
-                <img className={iconStyle} src={require("./trophy.svg")} />
-                <img className={iconStyle} src={require("./recycle.svg")} />
-                <img className={iconStyle} src={require("./cash.svg")} />
-                <img className={iconStyle} src={require("./account.svg")} />
-            </div>
-        );*/}
     }
 }
 
