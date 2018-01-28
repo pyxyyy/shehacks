1f0998b2b8ec7a97e854de06aaed0ebb6171d93f
drashtizglue <drashti@zglue.com>
2018-01-27 20:38:31 -0500

    Added banana.jpg. Added callAPI.js.

---
+++ src/backend/callAPI.js
@@ -0,0 +1,22 @@
+function analysis(filepath) {
+
+
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+
+    // Performs label detection on the image file
+    client
+        .labelDetection(filepath)
+        .then(results => {
+            const labels = results[0].labelAnnotations;
+
+            console.log('Labels:');
+            labels.forEach(label => console.log(label.description));
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+        });
+}
